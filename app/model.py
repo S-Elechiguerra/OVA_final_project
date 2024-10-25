d61572991e94efb037163624f0c9dcc91b57a008
S.Elechiguerra <s.elechiguerra@outlook.com>
2024-10-25 09:08:45 +0200

    web

--- app/model.py
+++
@@ -1,6 +0,0 @@
-import pickle
-# Function to load the pre-trained model
-def load_model():
-    with open('model/logistic_regression_model.pkl', 'rb') as file:
-        model = pickle.load(file)
-    return model
